4a26ac872456233252be2191b7ef111212c7769e
Pradnya <pathadepradnya09@gmail.com>
2021-02-24 13:41:41 +0530

    db change

--- database/dbconnection.php
+++ database/dbconnection.php
@@ -1,7 +1,18 @@
 <?php
 	// $db=mysqli_connect("192.168.0.101","stn","root","nks2");
 	// $db = mysqli_connect("localhost","root","","nks2");
-	$db=mysqli_connect("localhost","NKS","gSIUWxcV%o;L","NKS_Perfume");
+	//$db=mysqli_connect("localhost","NKS","gSIUWxcV%o;L","NKS_Perfume");
+
+// 	$myServer = "10.85.80.229";
+// $myUser = "root";
+// $myPass = "pass";
+// $myDB = "testdb";
+
+// $dbhandle = mssql_connect($myServer, $myUser, $myPass)
+//   or die("Couldn't connect to SQL Server on $myServer"); 
+
+$db=mysqli_connect("103.195.83.104
+","root","","signature_concepts");
 	if(!$db)
 	{
 	?>
